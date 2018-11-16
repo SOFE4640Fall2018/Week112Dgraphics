9db1600cbfff6df2a69aa449848ea2de3f98b4c7
AnwarBari <33730047+AnwarBari@users.noreply.github.com>
2018-11-15 19:39:42 -0500

    first commit

---
+++ app/src/main/java/ca/uoit/csci4100/samples/graphics2dwithandroid_sample/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.uoit.csci4100.samples.graphics2dwithandroid_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
